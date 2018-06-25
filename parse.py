3fc59b37079297a6f0357553fde320c6830c4696
xxl124730@utdallas.edu <Xia@ubuntu14.04LTS>
2018-06-25 12:38:00 -0500

    update parse.py

--- parse.py
+++ parse.py
@@ -6,36 +6,36 @@ import sys
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
-years = sys.argv[1]
+year = sys.argv[1]
 rootpath = "/home/Xia/XiaLi/GitHubProjects/1Archive/"
 writepath = "/home/Xia/XiaLi/GitHubProjects/2Parse/"
 urlandCommit = set()
 
 filecount = 1
-for year in years:      
-    filepath = rootpath + year
-    allfiles = os.listdir(filepath)
-    for file in allfiles:
-        print("file count: " + str(filecount) + " files")
-        filecount = filecount + 1       
-        with gzip.open(filepath + "/" + file, "rb") as f:
-            for line in f.readlines():
-                d = json.loads(line.decode("utf-8"))        
-                if "repository" in d:
-                    if "language" in d["repository"] and d["repository"]["language"] == "Java":
-                            url =  d["repository"]["url"]                           
-                            if "payload" in d and "shas" in d["payload"]:
-                                commits = d["payload"]["shas"]                
-                                for commit in commits:
-                                    message = commit[2]
-                                    if re.search(r'^(?=.*(bug|failure|issue|error|fault|defect|flaw|glitch))(?=.*(fix|solve|repair)).+$', message, re.IGNORECASE):  
-                                        urlcommit = url + " " + commit[0]                                
-                                        if urlcommit not in urlandCommit:
-                                            urlandCommit.add(urlcommit)
-                                            with open(writepath + "/commitandmessage/" + year + ".txt", "a") as writefile:
-                                                urlcommit = urlcommit.replace("//","//api.").replace(".com/",".com/repos/")
-                                                writefile.write(urlcommit + " [" + message + "]")
-                                                writefile.write("\n")
+     
+filepath = rootpath + year
+allfiles = os.listdir(filepath)
+for file in allfiles:
+    print("file count: " + str(filecount) + " files")
+    filecount = filecount + 1       
+    with gzip.open(filepath + "/" + file, "rb") as f:
+        for line in f.readlines():
+            d = json.loads(line.decode("utf-8"))        
+            if "repository" in d:
+                if "language" in d["repository"] and d["repository"]["language"] == "Java":
+                        url =  d["repository"]["url"]                           
+                        if "payload" in d and "shas" in d["payload"]:
+                            commits = d["payload"]["shas"]                
+                            for commit in commits:
+                                message = commit[2]
+                                if re.search(r'^(?=.*(bug|failure|issue|error|fault|defect|flaw|glitch))(?=.*(fix|solve|repair)).+$', message, re.IGNORECASE):  
+                                    urlcommit = url + " " + commit[0]                                
+                                    if urlcommit not in urlandCommit:
+                                        urlandCommit.add(urlcommit)
+                                        with open(writepath + "/commitandmessage/" + year + ".txt", "a") as writefile:
+                                            urlcommit = urlcommit.replace("//","//api.").replace(".com/",".com/repos/")
+                                            writefile.write(urlcommit + " [" + message + "]")
+                                            writefile.write("\n")
                 
 
         
