1a881339e45c8832d635f2b0a15dac2ab43dd3df
Xia <xia@testinglab.utdallas.edu>
2018-08-11 16:38:37 -0500

    update

--- checkDuplicate.py
+++ checkDuplicate.py
@@ -2,17 +2,17 @@ import os
 import sys
 
 def listfiles(partationpath,years):
-    newCommits = set()
+    newCommits = dict()
     count = 0
     for yearFile in years:
         oldfiles = os.listdir(partationpath + yearFile)
-
         for file in oldfiles:
             File2Path = partationpath + yearFile + "/" + file
             with open(File2Path) as newf:
                 for line in newf:
-                    urlCommit = line.split(" [")[0].strip().replace(".git "," ")
-                    newCommits.add(urlCommit)
+                    url = line.split(" ")[0]
+                    shaAndCommit = line.split(url + " ")[1].strip()
+                    newCommits[shaAndCommit] = line.strip().replace(".git "," ")
                     count = count + 1
 
     print(count)
@@ -20,10 +20,10 @@ def listfiles(partationpath,years):
     return newCommits
 
 
-partationpath = "/home/Xia/XiaLi/GitHubProjects/2Parse/partations/"
+partationpath = "/media/disk2/Xia/GitHubProjects/2Parse/commitandmessage/"
 years = ["2012-2014/","2011/","2015/","2017/","2016/"]
 BigCommits = listfiles(partationpath,years)
-print(list(BigCommits)[0])
+print(len(BigCommits))
 
 
 # path2016 = "/home/Xia/XiaLi/GitHubProjects/2Parse/commitandmessage/"
@@ -31,26 +31,26 @@ print(list(BigCommits)[0])
 # Commits2016 = listfiles(path2016,years2016)
 # print(list(Commits2016)[0])
 
-# partationSize = 8000
-# partionpath = "/home/Xia/XiaLi/GitHubProjects/2Parse/partations/2016/"
+partationSize = 10000
+partionpath = "/media/disk2/Xia/GitHubProjects/2Parse/partations/NewFull2011-2017"
 
 
-# count = 1       
-# fileNumber = 1  
-# uniqueCount = 0
-# for c in Commits2016:
-#     if c not in BigCommits:
-#         uniqueCount = uniqueCount + 1
-#         filename = partionpath + "/V" + str(fileNumber) + ".txt"
-#         with open(filename,"a") as writefile:
-#             writefile.write(c.strip())
-#             writefile.write("\n")
-#             if count < partationSize:               
-#                 count = count + 1
-#             else:
-#                 fileNumber = fileNumber + 1
-#                 count = 1
-# print(uniqueCount)
+count = 1       
+fileNumber = 1  
+uniqueCount = 0
+
+for key in BigCommits:
+    uniqueCount = uniqueCount + 1
+    filename = partionpath + "/V" + str(fileNumber) + ".txt"
+    with open(filename,"a") as writefile:
+        writefile.write(BigCommits[key].strip())
+        writefile.write("\n")
+        if count < partationSize:               
+            count = count + 1
+        else:
+            fileNumber = fileNumber + 1
+            count = 1
+print(uniqueCount)
 
         
 
